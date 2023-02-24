699c2da9f55da42ef1213b8545c721a6ea073dae
Fitsbit <107160828+Fitsbit@users.noreply.github.com>
2023-02-24 00:33:42 -0800

    test

--- fitsbit.py
+++ fitsbit.py
@@ -16,7 +16,7 @@ year = now.year
 
 ##############
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(9999)
+bytes = random._urandom(1490)
 #############
 
 os.system("clear")
